4caccbabea08986e590e0771734235cdf47817aa
hunor64 <nhunor06@gmail.com>
2024-09-09 10:42:26 +0200

    Added new class

---
+++ Models/Felhasznalo.cs
@@ -0,0 +1,10 @@
+﻿namespace AdatbazisKapcsolat.Models
+{
+    public class Felhasznalo
+    {
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public int SzulEv { get; set; }
+        public string Email { get; set; }
+    }
+}
