a0e24b904ee4245a06e28ac2b36599f4edcd5267
hunor64 <nhunor06@gmail.com>
2024-09-09 11:51:56 +0200

    added database

---
+++ Migrations/20240909094817_sqlite.local_migration_859.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdatbazisKapcsolat.Migrations
+{
+    /// <inheritdoc />
+    public partial class sqlitelocal_migration_859 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Felhasznalok",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nev = table.Column<string>(type: "TEXT", nullable: false),
+                    SzulEv = table.Column<int>(type: "INTEGER", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Felhasznalok", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Felhasznalok");
+        }
+    }
+}
--- migration_script.sql
+++ migration_script.sql
@@ -3,3 +3,17 @@
     "ProductVersion" TEXT NOT NULL
 );
 
+BEGIN TRANSACTION;
+
+CREATE TABLE "Felhasznalok" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Felhasznalok" PRIMARY KEY AUTOINCREMENT,
+    "Nev" TEXT NOT NULL,
+    "SzulEv" INTEGER NOT NULL,
+    "Email" TEXT NOT NULL
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20240909094817_sqlite.local_migration_859', '8.0.8');
+
+COMMIT;
+
